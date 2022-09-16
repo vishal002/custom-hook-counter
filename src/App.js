5d849eceadd7ef2ce2bf1ccf68e24d0cf498b96b
Vishal Kumar <VKumar61@slb.com>
2022-09-16 18:36:12 +0530

    implimintation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+function useCounter(initialValue) {
+  const [count, setCount] = useState(initialValue);
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(initialValue);
+
+  return [count, increment, decrement, reset]
+}
 
 function App() {
+  const [count, increment, decrement, reset] = useCounter(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>counter value: {count}</h1>
+      <button onClick={increment} >Increment</button>
+      <button onClick={decrement} >Decrement</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
